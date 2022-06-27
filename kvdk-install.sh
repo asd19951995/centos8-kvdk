544494a0a56e9acaf70a376f1c1728b3354b490b
asd19951995 <91370317+asd19951995@users.noreply.github.com>
2022-06-27 13:54:27 +0800

    Update kvdk-install.sh

--- kvdk-install.sh
+++ kvdk-install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+export http_proxy=http://child-prc.intel.com:913
+export https_proxy=http://child-prc.intel.com:913
+export ftp_proxy=https://child-prc.intel.com:913
+export socks_proxy=http://child-prc.intel.com:913
+
+sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
+sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-*
+
 yum config-manager --add-repo /etc/yum.repos.d/CentOS-Linux-PowerTools.repo
 yum config-manager --set-enabled PowerTools
 
