a8648b0846c6e6b6be9f30bd71011bf75fa9b531
asd19951995 <91370317+asd19951995@users.noreply.github.com>
2022-06-27 13:54:46 +0800

    Delete centos8-proxy.sh

--- centos8-proxy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export http_proxy=http://child-prc.intel.com:913 
-export https_proxy=http://child-prc.intel.com:913
-export ftp_proxy=https://child-prc.intel.com:913
-export socks_proxy=http://child-prc.intel.com:913
-
-sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
-sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-*
