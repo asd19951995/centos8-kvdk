c9ce6140f385861c7ab6742d0cec9de2a73ef2f0
asd19951995 <91370317+asd19951995@users.noreply.github.com>
2022-06-27 13:54:59 +0800

    Delete kvdk.sh

--- kvdk.sh
+++
@@ -1,7 +0,0 @@
-yum install -y daxctl.x86_64 daxctl-devel.x86_64 daxctl-libs.x86_64
-
-yum install -y ndctl.x86_64 ndctl-devel.x86_64 ndctl-libs.x86_64
-
-yum install -y libatomic numactl
-
-pip3 install GitPython
